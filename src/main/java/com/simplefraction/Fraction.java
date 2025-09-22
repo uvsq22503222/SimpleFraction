c53750a2fa97f8535ed1e682ea19486145112a02
GE Shuning <shuning.ge2001@gmail.com>
2025-09-22 23:07:32 +0200

    5

---
+++ src/main/java/com/simplefraction/Fraction.java
@@ -0,0 +1,8 @@
+package com.simplefraction;
+
+public class Fraction {
+    @Override
+    public String toString() {
+        return "Je suis une fraction.";
+    }
+}
---
+++ src/main/java/com/simplefraction/Main.java
@@ -0,0 +1,9 @@
+package com.simplefraction;
+
+public class Main {
+    public static void main(String[] args) {
+        Fraction f = new Fraction();
+        assert "Je suis une fraction.".equals(f.toString());
+        System.out.println(f);
+    }
+}
